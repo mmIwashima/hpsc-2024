c1284e308af8ad209ceda3e412fd5e21f9ca1824
mmIwashima <166234383+mmIwashima@users.noreply.github.com>
2024-04-20 15:46:46 +0900

    Add files via upload

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -16,8 +16,27 @@ int main() {
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+//  for (int i=1; i<range; i++) 
+//    offset[i] = offset[i-1] + bucket[i-1];
+
+// fixed
+#pragma omp parallel for
+  for (int i=1; i<range; i++)
+    offset[i] = bucket[i-1];
+//  for (int i=1; i<range; i++)
+//    offset[i] += offset[i-1];
+  std::vector<int> offset_sub(range,0);
+#pragma omp parallel
+  for (int j=1; j<range; j<<=1){
+#pragma omp for
+    for (int i=0; i<range; i++)
+      offset_sub[i] = offset[i];
+#pragma omp for
+    for (int i=j; i<range; i++)
+      offset[i] += offset_sub[i-j];
+  }	
+
+#pragma omp parallel
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
