1ad26cad325c132c220337511abd4f5ab644064f
mmIwashima <166234383+mmIwashima@users.noreply.github.com>
2024-04-21 16:55:33 +0900

    Add files via upload

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -8,7 +8,7 @@ struct Body {
 };
 
 int main(int argc, char** argv) {
-  const int N = 20;
+  const int N = 20;    //10000;
   MPI_Init(&argc, &argv);
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -21,14 +21,21 @@ int main(int argc, char** argv) {
     ibody[i].m = jbody[i].m = drand48();
     ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
   }
-  int recv_from = (rank + 1) % size;
+//  int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
+//    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    MPI_Win win;
+    MPI_Win_create(jbody, (N/size)*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody, N/size, MPI_INT, send_to, 0, N/size, MPI_INT, win);
+    MPI_Win_fence(0, win);
+
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
